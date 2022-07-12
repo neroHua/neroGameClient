d3e59468e10a786e2b29435a69a78836a7296012
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-12 18:10:52 +0800

    Create LocalStorageConstant.ts

---
+++ assets/typeScript/constant/LocalStorageConstant.ts
@@ -0,0 +1,15 @@
+
+export default class LocalStorageConstant {
+
+  /**
+   * 注册信息 key
+   */
+  public static readonly REGISTER_INFORMATION : string = "registerInformation";
+  
+  /**
+   * 用户信息 key
+   */
+  
+  public static readonly USER_INFORMATION : string = "userInformation";
+
+}
