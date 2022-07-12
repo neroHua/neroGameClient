bfff436cc3b20621f9537fb29d51ba0ab40d4f7d
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-12 13:55:03 +0800

    Create SceneConstant.ts

---
+++ assets/typeScript/constant/SceneConstant.ts
@@ -0,0 +1,13 @@
+export default class SceneConstant {
+
+  /**
+   * 登录管理器场景
+   */
+  public static readonly LOGIN_MANAGER_SCENE_URL: string = "scene/loginManagerScene";
+
+  /**
+   * 注册场景
+   */
+  public static readonly REGISTER_SCENE_URL: string = "scene/registerScene";
+
+}
