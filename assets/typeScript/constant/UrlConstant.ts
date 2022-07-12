0cf8f48c36bf4d70b2279cdcce50ec21ddba961b
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-12 18:06:25 +0800

    Update UrlConstant.ts

--- assets/typeScript/constant/UrlConstant.ts
+++ assets/typeScript/constant/UrlConstant.ts
@@ -1,5 +1,7 @@
 export default class UrlConstant {
 
-  public static readonly REGISTER_URL = 'user/register'
+  public static readonly REGISTER_URL : string = 'user/register';
+
+  public static readonly LOGIN_URL : string = 'user/login';
 
 }
