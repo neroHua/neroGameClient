875067c3674065be3f30f93efbff90179ce068a5
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-12 18:05:54 +0800

    Update SceneConstant.ts

--- assets/typeScript/constant/SceneConstant.ts
+++ assets/typeScript/constant/SceneConstant.ts
@@ -10,4 +10,14 @@ export default class SceneConstant {
    */
   public static readonly REGISTER_SCENE_URL: string = "scene/registerScene";
 
+  /**
+   * 登录场景
+   */
+  public static readonly LOGIN_SCENE_URL: string = "scene/loginScene";
+
+  /**
+   * 大厅场景
+   */
+  public static readonly HALL_SCENE_URL: string = "scene/hallScene";
+
 }
