63ea69e92f37c08480c48d9ce9df4e01d5131fe6
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-12 16:44:18 +0800

    Create HttpServerConfig.ts

---
+++ assets/typeScript/config/HttpServerConfig.ts
@@ -0,0 +1,8 @@
+export default class ServerConfig {
+
+  public static readonly HOST: string = 'localhost';
+
+  public static readonly PORT: string = '8080';
+
+  public static readonly HTTP_URL_PRE_FIX = 'http://localhost:8080/';
+}
