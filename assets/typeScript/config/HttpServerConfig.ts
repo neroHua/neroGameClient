3b08a78baddca128bd6ae671014542173960c0f0
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-13 18:28:27 +0800

    Update HttpServerConfig.ts

--- assets/typeScript/config/HttpServerConfig.ts
+++ assets/typeScript/config/HttpServerConfig.ts
@@ -2,7 +2,7 @@ export default class ServerConfig {
 
   public static readonly HOST: string = 'localhost';
 
-  public static readonly PORT: string = '8080';
+  public static readonly PORT: string = '8082';
 
-  public static readonly HTTP_URL_PRE_FIX = 'http://localhost:8080/';
+  public static readonly HTTP_URL_PRE_FIX = 'http://localhost:8082/';
 }
