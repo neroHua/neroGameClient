41654ea538c21b7090da56f81b43d5fe032fb844
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-14 11:44:33 +0800

    Update HttpResponse.ts

--- assets/typeScript/bean/http/HttpResponse.ts
+++ assets/typeScript/bean/http/HttpResponse.ts
@@ -1,5 +1,21 @@
 import BaseBean from "../BaseBean";
 
-export default class HttpResponse extends BaseBean {
+export default class HttpResponse <T> extends BaseBean {
+
+  public success : boolean;
+
+  public errorCode : string;
+
+  public errorMessage : string;
+
+  public data : T;
+
+  constructor(success : boolean , errorCode : string, errorMessage : string, data : T) {
+    super();
+    this.success = success;
+    this.errorCode = errorCode;
+    this.errorMessage = errorMessage;
+    this.data = data;
+  }
 
 }
