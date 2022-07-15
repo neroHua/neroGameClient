bf02b85479d980da8ea0cd953126cff6a8e8aa0e
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-15 16:37:58 +0800

    Create HallController.ts

---
+++ assets/typeScript/HallController.ts
@@ -0,0 +1,21 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class HallController extends cc.Component {
+
+    // onLoad () {}
+
+    start () {
+
+    }
+
+    // update (dt) {}
+    
+    onCreateRoom() {
+
+    }
+
+    onJoinRoom() {
+
+    }
+}
