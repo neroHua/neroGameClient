82b49a8b700193516e208dfe0a63b3142ed30615
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-18 16:22:14 +0800

    Update HttpManager.ts

--- assets/typeScript/net/HttpManager.ts
+++ assets/typeScript/net/HttpManager.ts
@@ -1,8 +1,9 @@
 import HttpRequest from "../bean/http/HttpRequest";
 import HttpResponse from "../bean/http/HttpResponse";
 import HttpServerConfig from "../config/HttpServerConfig";
+import LocalStorageConstant from "../constant/LocalStorageConstant";
 
-export default class HttpManager {
+export default class HttpManager extends cc.Component {
 
   private static readonly GET : string = 'GET';
   private static readonly POST : string = 'POST';
@@ -17,12 +18,13 @@ export default class HttpManager {
       }
     }
     if (null == requestBean) {
-      request.open(HttpManager.POST, HttpServerConfig.HTTP_URL_PRE_FIX + url, false);
+      request.open(HttpManager.GET, HttpServerConfig.HTTP_URL_PRE_FIX + url, false);
     }
     else {
       request.open(HttpManager.GET, HttpServerConfig.HTTP_URL_PRE_FIX + url + '?' + this.beanToURLString(requestBean), false);
     }
     request.setRequestHeader("Content-type","application/json");
+    request.setRequestHeader(LocalStorageConstant.TOKEN, cc.sys.localStorage.getItem(LocalStorageConstant.TOKEN));
     request.send(null);
     return JSON.parse(responseText);
   }
@@ -38,6 +40,7 @@ export default class HttpManager {
     }
     request.open(HttpManager.POST, HttpServerConfig.HTTP_URL_PRE_FIX + url, false);
     request.setRequestHeader("Content-type","application/json");
+    request.setRequestHeader("token", cc.sys.localStorage.getItem("token"));
     request.send(JSON.stringify(requestBean));
     return JSON.parse(responseText);
   }
