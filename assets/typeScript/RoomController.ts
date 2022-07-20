5e0fac4101edb15736daf1462ed89c88de442050
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-20 16:59:41 +0800

    Create RoomController.ts

---
+++ assets/typeScript/RoomController.ts
@@ -0,0 +1,30 @@
+import WebSocketManager from "./net/WebSocketManager";
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class RoomController extends cc.Component {
+
+    @property(cc.Node)
+    private prepareButton: cc.Node = null;
+
+    private prepareStatus : boolean = null;
+
+    start () {
+      WebSocketManager.init();
+      this.prepareStatus = true;
+    }
+
+    public changePrepareStatus() : void {
+      this.prepareStatus = !this.prepareStatus;
+      
+      let prepareNode : cc.Node = this.node.getChildByName('prepare');
+      if (this.prepareStatus) {
+        prepareNode.children[0].getComponent(cc.Label).string = "取消准备";
+      }
+      else {
+        prepareNode.children[0].getComponent(cc.Label).string = "准备";
+      }
+    }
+
+}
