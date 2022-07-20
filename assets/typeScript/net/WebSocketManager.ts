94890838a5c588c54bf5bc188ab5bd493adca47c
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-20 13:43:29 +0800

    Create WebSocketManager.ts

---
+++ assets/typeScript/net/WebSocketManager.ts
@@ -0,0 +1,40 @@
+import WebSocketConfig from "../config/WebSocketConfig";
+import LocalStorageConstant from "../constant/LocalStorageConstant";
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class WebSocketManager extends cc.Component {
+
+  private static webSocket : WebSocket = null;
+
+  public static init() {
+    let userInformation = cc.sys.localStorage.getItem(LocalStorageConstant.USER_INFORMATION);
+
+    this.webSocket = new WebSocket(WebSocketConfig.HTTP_URL_PRE_FIX + userInformation.userId);
+    this.webSocket.onopen = this.onOpen;
+    this.webSocket.onclose= this.onClose;
+    this.webSocket.onerror= this.onError;
+    this.webSocket.onmessage= this.onMessage;
+  }
+
+  private static onOpen() {
+    console.log('链接成功');
+  }
+
+  private static onClose() {
+    console.log('链接关闭');
+  }
+
+  private static onError() {
+    console.log('链接出错');
+  }
+  public static onMessage(message) {
+    console.log(message, '获得消息');
+  }
+
+  public static sendMessage(message : string) {
+    this.webSocket.send(message);
+  }
+
+}
