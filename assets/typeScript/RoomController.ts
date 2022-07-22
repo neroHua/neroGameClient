7e7d8e5bc815117d21209affb49a262e354419df
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-22 14:31:27 +0800

    Update RoomController.ts

--- assets/typeScript/RoomController.ts
+++ assets/typeScript/RoomController.ts
@@ -1,3 +1,6 @@
+import UserChangePreparedStatusRequest from "./bean/http/user/UserChangePreparedStatusRequest";
+import UrlConstant from "./constant/UrlConstant";
+import HttpManager from "./net/HttpManager";
 import WebSocketManager from "./net/WebSocketManager";
 
 const {ccclass, property} = cc._decorator;
@@ -10,14 +13,34 @@ export default class RoomController extends cc.Component {
 
     private prepareStatus : boolean = null;
 
+    private webSocketManager : WebSocketManager = null;
+
     start () {
-      WebSocketManager.init();
+      this.webSocketManager = new WebSocketManager();
+      this.webSocketManager.init(this.onOpen, this.onClose, this.onError, this.onMessage);
       this.prepareStatus = true;
+    }
+
+    private onOpen() {
+      console.log('链接成功');
+    }
+  
+    private onClose() {
+      console.log('链接关闭');
+    }
+  
+    private onError() {
+      console.log('链接出错');
+    }
+
+    public onMessage(message) {
+      console.log(message, '获得消息');
     }
 
     public changePrepareStatus() : void {
       this.prepareStatus = !this.prepareStatus;
-      WebSocketManager.sendMessage("change the status");
+      let userChangePreparedStatusRequest : UserChangePreparedStatusRequest = new UserChangePreparedStatusRequest(this.prepareStatus);
+      HttpManager.post(userChangePreparedStatusRequest, UrlConstant.ROOM_CHANGE_USER_PREPARE_STATUS);
 
       let prepareNode : cc.Node = this.node.getChildByName('prepare');
       if (this.prepareStatus) {
@@ -26,6 +49,7 @@ export default class RoomController extends cc.Component {
       else {
         prepareNode.children[0].getComponent(cc.Label).string = "准备";
       }
+
     }
 
 }
