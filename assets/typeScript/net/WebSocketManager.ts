b0f354aa87b70d8b410ae6eee51ee3f7084db1ff
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-22 14:33:05 +0800

    Update WebSocketManager.ts

--- assets/typeScript/net/WebSocketManager.ts
+++ assets/typeScript/net/WebSocketManager.ts
@@ -6,9 +6,9 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class WebSocketManager extends cc.Component {
 
-  private static webSocket : WebSocket = null;
+  private webSocket : WebSocket = null;
 
-  public static init() {
+  public init(onOpen, onClose, onError, onMessage) {
     let userInformationString = cc.sys.localStorage.getItem(LocalStorageConstant.USER_INFORMATION);
     let userInformation = JSON.parse(userInformationString);
     console.log(userInformationString);
@@ -16,28 +16,13 @@ export default class WebSocketManager extends cc.Component {
 
     this.webSocket = new WebSocket(WebSocketConfig.HTTP_URL_PRE_FIX + userInformation.userId);
 
-    this.webSocket.onopen = this.onOpen;
-    this.webSocket.onclose= this.onClose;
-    this.webSocket.onerror= this.onError;
-    this.webSocket.onmessage= this.onMessage;
+    this.webSocket.onopen = onOpen;
+    this.webSocket.onclose = onClose;
+    this.webSocket.onerror = onError;
+    this.webSocket.onmessage = onMessage;
   }
 
-  private static onOpen() {
-    console.log('链接成功');
-  }
-
-  private static onClose() {
-    console.log('链接关闭');
-  }
-
-  private static onError() {
-    console.log('链接出错');
-  }
-  public static onMessage(message) {
-    console.log(message, '获得消息');
-  }
-
-  public static sendMessage(message : string) {
+  public sendMessage(message : string) {
     this.webSocket.send(message);
   }
 
