dbe2cfef38e830bb0a95a33dc2bcb00dfcea5526
neroHua <38644203+neroHua@users.noreply.github.com>
2022-07-26 15:43:23 +0800

    Create RoomConstant.ts

---
+++ assets/typeScript/constant/RoomConstant.ts
@@ -0,0 +1,17 @@
+export default class RoomConstant {
+
+  public static readonly USER_NODE_NAME_ME: string = "user0";
+
+  public static readonly USER_NODE_NAME_PREFIX : string = "user";
+
+  public static readonly USER_INFORMATION_NODE_NAME : string = "userInformation";
+
+  public static readonly PREPARE_NODE_NAME : string = "prepare";
+
+  public static readonly PREPARE_NODE_LABEL_YES : string = "取消准备";
+
+  public static readonly PREPARE_NODE_LABEL_NO : string = "准备";
+
+  public static readonly CARD_LIST_NODE_NAME : string = "cardList";
+
+}
