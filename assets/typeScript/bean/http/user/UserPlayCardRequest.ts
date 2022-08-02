24f1e186c7969b7d0789062ab52a30a98303152c
neroHua <38644203+neroHua@users.noreply.github.com>
2022-08-02 17:31:53 +0800

    Create UserPlayCardRequest.ts

---
+++ assets/typeScript/bean/http/user/UserPlayCardRequest.ts
@@ -0,0 +1,9 @@
+export default class UserPlayCardRequest {
+
+  private cardEnumerationList : Array<string>;
+
+  constructor(cardEnumerationList : Array<string>) {
+    this.cardEnumerationList = cardEnumerationList;
+  }
+
+}
