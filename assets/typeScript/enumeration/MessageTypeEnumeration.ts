baeb8e7af9ac0a6fdd0918189ca79d0f7848cccd
neroHua <38644203+neroHua@users.noreply.github.com>
2022-08-02 11:04:00 +0800

    Update MessageTypeEnumeration.ts

--- assets/typeScript/enumeration/MessageTypeEnumeration.ts
+++ assets/typeScript/enumeration/MessageTypeEnumeration.ts
@@ -29,10 +29,20 @@ export default class MessageType {
 export const MessageTypeEnumeration = { 
   USER_JOIN_ROOM: new MessageType("USER_JOIN_ROOM", "userJoinRoom", "用户加入房间"),
   USER_LEAVE_ROOM: new MessageType("USER_LEAVE_ROOM", "userLeaveRoom", "用户离开房间"),
+
   CHANGE_USER_PREPARE_STATUS: new MessageType("CHANGE_USER_PREPARE_STATUS", "changeUserPrepareStatus", "用户改变准备状态"),
+
   DEAL_CARD: new MessageType("DEAL_CARD", "dealCard", "发牌"),
+
   USER_START_ROB_LANDLORD : new MessageType("USER_START_ROB_LANDLORD", "userStartRobLandlord", "用户开始抢地主消息"),
   USER_DO_ROB_LANDLORD : new MessageType("USER_DO_ROB_LANDLORD", "userDoRobLandlord", "用户抢地主消息"),
   USER_DO_NOT_ROB_LANDLORD : new MessageType("USER_DO_NOT_ROB_LANDLORD", "userDoNotRobLandlord", "用户不抢地主消息"),
+
   DEAL_LANDLORD_CARD : new MessageType("DEAL_LANDLORD_CARD", "dealLandlordCard", "发地主牌"),
+
+  USER_START_TO_PLAY_CARD : new MessageType("USER_START_TO_PLAY_CARD", "userStartToPlayCard", "用户开始出牌"),
+  USER_DO_PLAY_CARD : new MessageType("USER_DO_PLAY_CARD", "useDoPlayCard", "用户出牌"),
+  USER_DO_NOT_PLAY_CARD : new MessageType("USER_DO_NOT_PLAY_CARD", "userDoNotPlayCard", "用户不出牌"),
+
+  USER_WIN : new MessageType("USER_WIN", "userWin", "用户赢得一局"),
 }
