96c91f27a659d8784e2197202948189535e8ec13
neroHua <38644203+neroHua@users.noreply.github.com>
2022-08-09 14:46:33 +0800

    Create AirplaneSingleValidate.ts

---
+++ assets/typeScript/validate/impl/AirplaneSingleValidate.ts
@@ -0,0 +1,44 @@
+import CardEnumeration from "../../enumeration/CardEnumeration";
+import PlayCardType from "../../enumeration/PlayCardTypeEnumeration";
+import PlayCardTypeEnumeration from "../../enumeration/PlayCardTypeEnumeration";
+import PlayCardTypeValidate from "../PlayCardTypeValidate";
+
+export default class AirplaneSingleValidate implements PlayCardTypeValidate {
+
+  private readonly MIN_COUNT : number = 8;
+  private readonly GROUP_COUNT : number = 4;
+
+  getPlayCardTypeEnumeration(): PlayCardType {
+    return PlayCardTypeEnumeration['AIRPLANE_SINGLE'];
+  }
+
+  match(cardEnumerationList: Array<CardEnumeration>): boolean {
+    if (cardEnumerationList.length < this.MIN_COUNT) {
+      return false;
+    }
+
+    if (0 != cardEnumerationList.length % this.GROUP_COUNT) {
+      return false;
+    }
+
+    for (let i : number = 0; i < cardEnumerationList.length / this.GROUP_COUNT; i += 3) {
+      if (cardEnumerationList[i].getValue() != cardEnumerationList[i + 1].getValue()
+          || cardEnumerationList[i + 1].getValue() != cardEnumerationList[i + 2].getValue()) {
+        return false;
+      }
+    }
+
+    for (let i : number = 0; i < cardEnumerationList.length / this.GROUP_COUNT; i += 3) {
+      if (cardEnumerationList[i].getValue() - 1 != cardEnumerationList[i + 3].getValue()) {
+        return false;
+      }
+    }
+
+    if (cardEnumerationList[0].getValue() >= CardEnumeration['CARD_415'].getValue()) {
+      return false;
+    }
+
+    return true;
+  }
+
+}
