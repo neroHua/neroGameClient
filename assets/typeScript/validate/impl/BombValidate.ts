ed1b8d6f261cd264e5486784a8e87531fe4943f4
neroHua <38644203+neroHua@users.noreply.github.com>
2022-08-09 16:40:46 +0800

    Update BombValidate.ts

--- assets/typeScript/validate/impl/BombValidate.ts
+++ assets/typeScript/validate/impl/BombValidate.ts
@@ -1,6 +1,5 @@
-import CardEnumeration from "../../enumeration/CardEnumeration";
-import PlayCardType from "../../enumeration/PlayCardTypeEnumeration";
-import PlayCardTypeEnumeration from "../../enumeration/PlayCardTypeEnumeration";
+import Card, { CardEnumeration } from "../../enumeration/CardEnumeration";
+import PlayCardType, { PlayCardTypeEnumeration } from "../../enumeration/PlayCardTypeEnumeration";
 import PlayCardTypeValidate from "../PlayCardTypeValidate";
 
 export default class BombValidate implements PlayCardTypeValidate {
@@ -8,10 +7,10 @@ export default class BombValidate implements PlayCardTypeValidate {
   private readonly COUNT : number = 4;
 
   getPlayCardTypeEnumeration(): PlayCardType {
-    return PlayCardTypeEnumeration['BOMB'];
+    return PlayCardTypeEnumeration.BOMB;
   }
 
-  match(cardEnumerationList: Array<CardEnumeration>): boolean {
+  match(cardEnumerationList: Array<Card>): boolean {
     if (this.COUNT != cardEnumerationList.length) {
       return false;
     }
