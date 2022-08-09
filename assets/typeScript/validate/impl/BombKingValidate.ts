259b2deccf352eb79f28051fad2092b66aaa7767
neroHua <38644203+neroHua@users.noreply.github.com>
2022-08-09 16:40:20 +0800

    Update BombKingValidate.ts

--- assets/typeScript/validate/impl/BombKingValidate.ts
+++ assets/typeScript/validate/impl/BombKingValidate.ts
@@ -1,6 +1,5 @@
-import CardEnumeration from "../../enumeration/CardEnumeration";
-import PlayCardType from "../../enumeration/PlayCardTypeEnumeration";
-import PlayCardTypeEnumeration from "../../enumeration/PlayCardTypeEnumeration";
+import Card, { CardEnumeration } from "../../enumeration/CardEnumeration";
+import PlayCardType, { PlayCardTypeEnumeration } from "../../enumeration/PlayCardTypeEnumeration";
 import PlayCardTypeValidate from "../PlayCardTypeValidate";
 
 export default class BombKingValidate implements PlayCardTypeValidate {
@@ -8,16 +7,16 @@ export default class BombKingValidate implements PlayCardTypeValidate {
   private readonly COUNT : number = 2;
 
   getPlayCardTypeEnumeration(): PlayCardType {
-    return PlayCardTypeEnumeration['BOMB_KING'];
+    return PlayCardTypeEnumeration.BOMB_KING;
   }
 
-  match(cardEnumerationList: Array<CardEnumeration>): boolean {
+  match(cardEnumerationList: Array<Card>): boolean {
     if (this.COUNT != cardEnumerationList.length) {
       return false;
     }
 
-    return cardEnumerationList[0].getValue() === CardEnumeration['CARD_516'].getValue()
-      && cardEnumerationList[1].getValue() === CardEnumeration['CARD_517'].getValue();
+    return cardEnumerationList[0].getValue() === CardEnumeration.CARD_516.getValue()
+      && cardEnumerationList[1].getValue() === CardEnumeration.CARD_517.getValue();
   }
 
 }
