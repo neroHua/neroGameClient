4eef583824202b1a52d1adaff9f9dd3986e96c1c
neroHua <38644203+neroHua@users.noreply.github.com>
2022-08-09 14:50:59 +0800

    Create StraightValidate.ts

---
+++ assets/typeScript/validate/impl/StraightValidate.ts
@@ -0,0 +1,32 @@
+import CardEnumeration from "../../enumeration/CardEnumeration";
+import PlayCardType from "../../enumeration/PlayCardTypeEnumeration";
+import PlayCardTypeEnumeration from "../../enumeration/PlayCardTypeEnumeration";
+import PlayCardTypeValidate from "../PlayCardTypeValidate";
+
+export default class StraightValidate implements PlayCardTypeValidate {
+
+  private readonly MIN_COUNT : number = 5;
+
+  getPlayCardTypeEnumeration(): PlayCardType {
+    return PlayCardTypeEnumeration['STRAIGHT'];
+  }
+
+  match(cardEnumerationList: Array<CardEnumeration>): boolean {
+    if (cardEnumerationList.length < this.MIN_COUNT) {
+      return false;
+    }
+
+    for (let i : number = 1; i < cardEnumerationList.length; i++) {
+      if (cardEnumerationList[i].getValue() + 1 != cardEnumerationList[i - 1].getValue()) {
+          return false;
+      }
+    }
+
+    if (cardEnumerationList[0].getValue() >= CardEnumeration['CARD_415'].getValue()) {
+      return false;
+    }
+
+    return true;
+  }
+
+}
