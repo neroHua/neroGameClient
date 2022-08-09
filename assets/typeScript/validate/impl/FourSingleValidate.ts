fe71c343e9928d3486b6f8dc6b83106da4c974dd
neroHua <38644203+neroHua@users.noreply.github.com>
2022-08-09 14:48:53 +0800

    Create FourSingleValidate.ts

---
+++ assets/typeScript/validate/impl/FourSingleValidate.ts
@@ -0,0 +1,24 @@
+import CardEnumeration from "../../enumeration/CardEnumeration";
+import PlayCardType from "../../enumeration/PlayCardTypeEnumeration";
+import PlayCardTypeEnumeration from "../../enumeration/PlayCardTypeEnumeration";
+import PlayCardTypeValidate from "../PlayCardTypeValidate";
+
+export default class FourSingleValidate implements PlayCardTypeValidate {
+
+  private readonly COUNT : number = 6;
+
+  getPlayCardTypeEnumeration(): PlayCardType {
+    return PlayCardTypeEnumeration['FOUR_SINGLE'];
+  }
+
+  match(cardEnumerationList: Array<CardEnumeration>): boolean {
+    if (this.COUNT != cardEnumerationList.length) {
+      return false;
+    }
+
+    return cardEnumerationList[0].getValue() === cardEnumerationList[1].getValue()
+      && cardEnumerationList[1].getValue() === cardEnumerationList[2].getValue()
+      && cardEnumerationList[2].getValue() === cardEnumerationList[3].getValue();
+  }
+
+}
