a204c1ffb013d5ec5bf3e4d797188c01fd3860be
neroHua <38644203+neroHua@users.noreply.github.com>
2022-08-09 14:48:26 +0800

    Create FourPairValidate.ts

---
+++ assets/typeScript/validate/impl/FourPairValidate.ts
@@ -0,0 +1,26 @@
+import CardEnumeration from "../../enumeration/CardEnumeration";
+import PlayCardType from "../../enumeration/PlayCardTypeEnumeration";
+import PlayCardTypeEnumeration from "../../enumeration/PlayCardTypeEnumeration";
+import PlayCardTypeValidate from "../PlayCardTypeValidate";
+
+export default class FourPairValidate implements PlayCardTypeValidate {
+
+  private readonly COUNT : number = 8;
+
+  getPlayCardTypeEnumeration(): PlayCardType {
+    return PlayCardTypeEnumeration['FOUR_PAIR'];
+  }
+
+  match(cardEnumerationList: Array<CardEnumeration>): boolean {
+    if (this.COUNT != cardEnumerationList.length) {
+      return false;
+    }
+
+    return cardEnumerationList[0].getValue() === cardEnumerationList[1].getValue()
+      && cardEnumerationList[1].getValue() === cardEnumerationList[2].getValue()
+      && cardEnumerationList[2].getValue() === cardEnumerationList[3].getValue()
+      && cardEnumerationList[4].getValue() === cardEnumerationList[5].getValue()
+      && cardEnumerationList[6].getValue() === cardEnumerationList[7].getValue();
+  }
+
+}
