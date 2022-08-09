c46cce7d6b475bd91a125a2b724f81b6ce274b7d
neroHua <38644203+neroHua@users.noreply.github.com>
2022-08-09 14:49:37 +0800

    Create PairStraightValidate.ts

---
+++ assets/typeScript/validate/impl/PairStraightValidate.ts
@@ -0,0 +1,43 @@
+import CardEnumeration from "../../enumeration/CardEnumeration";
+import PlayCardType from "../../enumeration/PlayCardTypeEnumeration";
+import PlayCardTypeEnumeration from "../../enumeration/PlayCardTypeEnumeration";
+import PlayCardTypeValidate from "../PlayCardTypeValidate";
+
+export default class PairStraightValidate implements PlayCardTypeValidate {
+
+  private readonly MIN_COUNT : number = 6;
+  private readonly GROUP_COUNT : number = 2;
+
+  getPlayCardTypeEnumeration(): PlayCardType {
+    return PlayCardTypeEnumeration['PAIR_STRAIGHT'];
+  }
+
+  match(cardEnumerationList: Array<CardEnumeration>): boolean {
+    if (cardEnumerationList.length < this.MIN_COUNT) {
+      return false;
+    }
+
+    if (0 != cardEnumerationList.length % this.GROUP_COUNT) {
+      return false;
+    }
+
+    for (let i : number = 0; i < cardEnumerationList.length / this.GROUP_COUNT; i += 2) {
+        if (cardEnumerationList[i].getValue() != cardEnumerationList[i + 1].getValue()) {
+            return false;
+        }
+    }
+
+    for (let i = 0; i < cardEnumerationList.length / this.GROUP_COUNT; i += 2) {
+        if (cardEnumerationList[i].getValue() - 1 != cardEnumerationList[i + 2].getValue()) {
+            return false;
+        }
+    }
+
+    if (cardEnumerationList[0].getValue() >= CardEnumeration['CARD_415'].getValue()) {
+        return false;
+    }
+
+    return true;
+  }
+
+}
