d4d58f91d65107b60493dea2d7c3c19649013b48
neroHua <38644203+neroHua@users.noreply.github.com>
2022-08-09 16:42:37 +0800

    Update StraightValidate.ts

--- assets/typeScript/validate/impl/StraightValidate.ts
+++ assets/typeScript/validate/impl/StraightValidate.ts
@@ -1,6 +1,5 @@
-import CardEnumeration from "../../enumeration/CardEnumeration";
-import PlayCardType from "../../enumeration/PlayCardTypeEnumeration";
-import PlayCardTypeEnumeration from "../../enumeration/PlayCardTypeEnumeration";
+import Card, { CardEnumeration } from "../../enumeration/CardEnumeration";
+import PlayCardType, { PlayCardTypeEnumeration } from "../../enumeration/PlayCardTypeEnumeration";
 import PlayCardTypeValidate from "../PlayCardTypeValidate";
 
 export default class StraightValidate implements PlayCardTypeValidate {
@@ -8,10 +7,10 @@ export default class StraightValidate implements PlayCardTypeValidate {
   private readonly MIN_COUNT : number = 5;
 
   getPlayCardTypeEnumeration(): PlayCardType {
-    return PlayCardTypeEnumeration['STRAIGHT'];
+    return PlayCardTypeEnumeration.STRAIGHT;
   }
 
-  match(cardEnumerationList: Array<CardEnumeration>): boolean {
+  match(cardEnumerationList: Array<Card>): boolean {
     if (cardEnumerationList.length < this.MIN_COUNT) {
       return false;
     }
@@ -22,7 +21,7 @@ export default class StraightValidate implements PlayCardTypeValidate {
       }
     }
 
-    if (cardEnumerationList[0].getValue() >= CardEnumeration['CARD_415'].getValue()) {
+    if (cardEnumerationList[0].getValue() >= CardEnumeration.CARD_415.getValue()) {
       return false;
     }
 
